bfc2b6f85393956e79deb2380824407299908d87
Franco_Serjen <francoserjen100@gmail.com>
2023-10-03 11:48:32 -0300

    Creacion de los componentes principales

---
+++ src/app/calendario-de-asistencia/calendario-de-asistencia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendario-de-asistencia',
+  templateUrl: './calendario-de-asistencia.component.html',
+  styleUrls: ['./calendario-de-asistencia.component.css']
+})
+export class CalendarioDeAsistenciaComponent {
+
+}
