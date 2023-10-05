ee7fe2629fc4b5713912334ba26d3ba2b5d41d45
Franco_Serjen <francoserjen100@gmail.com>
2023-10-05 17:50:27 -0300

    Creo servicio Jugadores. Creo dos metodos, agregarJugador() donde guarda los jugadores creados y obtenerJugadores() donde obtenemos los jugadores guardados. Inyectamos el servicio Jugadores, creamos dos metodos, agregarJugadores() donde llamo al metodo del servicio que almacena los jugadores nuevos y reiniciarFormulario() donde reinicio el formulario. Importo FormsModule para trabajar con ngModule en html

--- src/app/nuevo-jugador/nuevo-jugador.component.ts
+++ src/app/nuevo-jugador/nuevo-jugador.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Jugador } from '../interfaces/Jugador';
+import { JugadoresService } from '../jugadores.service';
 
 @Component({
   selector: 'app-nuevo-jugador',
@@ -8,11 +9,31 @@ import { Jugador } from '../interfaces/Jugador';
 })
 export class NuevoJugadorComponent {
 
-   nuevoJugador: Jugador = {
+  //Creamos atributo nuevoJugador de tipo Jugador(interface) con valores en "";
+   private nuevoJugador: Jugador = {
      urlImagen: "",
      nombre: "",
      apellido: "",
      fechaDeNacimiento: "",
      club: "",
+   }
+
+   //Inyectamos el servicio creado
+   constructor(private jugadoresService:JugadoresService) { }
+  
+   //Metodo donde llamamos al servicio y traemos el metodo agregarJugador() donde almacena los jugadores creados
+  agregarJugadores() {
+    this.jugadoresService.agregarJugador(this.nuevoJugador);
+   };
+
+   //Metodo creado para reiniciar el formulario una vez completado el mismo
+   reiniciarFormulario(){
+    this.nuevoJugador = {
+      urlImagen: "",
+      nombre: "",
+      apellido: "",
+      fechaDeNacimiento: "",
+      club: "",
+    }
    }
 }
