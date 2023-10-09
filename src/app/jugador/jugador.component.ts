4b515ad62b42ca4835fe28ef0b91996c78fc8cc1
Franco_Serjen <francoserjen100@gmail.com>
2023-10-09 17:33:54 -0300

    Se implementa el formulario para mostrar la informacion que se obtiene del servicio. Se inyecta el servicio Jugadores. Creamos componente recibirJugador, donde se llama al componente obtenerJugadores del servicio inyectado y almacenamos los jugadores que obtenemos del servicio en el arreglo de jugador. Se implementa la interfaz OnInit para llamar al metodo recibirJugador. Importamos Router (las rutas). Creamos metodo volverAJugadores, donde llamamos el atributo de las rutas para navegar (volcer) al componente jugadores

--- src/app/jugador/jugador.component.ts
+++ src/app/jugador/jugador.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JugadoresService } from '../JugadoresService';
+import { Jugador } from '../interfaces/Jugador';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-jugador',
   templateUrl: './jugador.component.html',
   styleUrls: ['./jugador.component.css']
 })
-export class JugadorComponent {
+export class JugadorComponent implements OnInit {
 
+  //Atributos de arreglo de jugadores para poder almacenarlos en algun lugar
+  jugador: Jugador[] = [];
+
+  //constructor
+  constructor(private jugadorService: JugadoresService, private router: Router) { }
+
+  //Metodo que se ejecuta despues del constructor
+  ngOnInit() {
+    this.recibirJugador();
+  }
+
+  //Metodo que llama al servicio para obtener los jugadores y almacenarlos en el arreglo jugador
+  recibirJugador() {
+    this.jugador = this.jugadorService.obtenerJugadores();
+  }
+
+  //Metodo que se aplical al boton "Volver", donde escucha el evento click y actua volviendo al componente jugadores
+  volverAJugadores() {
+    this.router.navigate(['/jugadores']);
+  }
 }
