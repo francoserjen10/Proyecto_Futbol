3e98141d95d6b87ae2ed353b86767aca7c254301
Franco_Serjen <francoserjen100@gmail.com>
2023-10-17 15:24:17 -0300

    Agrego el calendario en el html con los events las options (html). Elimino el strict (tsconfig.json). Instalo fullcalendar/angular, fullcalendar/core, fullcalendar/daygrid, fullcalendar/interaction, fullcalendar/timegrid, primeicons, primeng (package.json). Importo FullCalendarModule (appModule). Importo todo lo de fullcalendar, implemento la interface OnInit, creo evento y options que los llamo desde el html (calendario de asistencia . js)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { FullCalendarModule } from '@fullcalendar/angular'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,7 +27,8 @@ import { NuevoJugadorComponent } from './nuevo-jugador/nuevo-jugador.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calendario-de-asistencia/calendario-de-asistencia.component.ts
+++ src/app/calendario-de-asistencia/calendario-de-asistencia.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction';
+import esLocale from '@fullcalendar/core/locales/es'
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-calendario-de-asistencia',
   templateUrl: './calendario-de-asistencia.component.html',
   styleUrls: ['./calendario-de-asistencia.component.css']
 })
-export class CalendarioDeAsistenciaComponent {
+export class CalendarioDeAsistenciaComponent implements OnInit {
+  events: any[];
+  options: any;
 
+  constructor() { }
+
+  ngOnInit() {
+
+    this.options = {
+      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+      defaultDate: new Date(),
+      locale: esLocale,
+      headerToolbar: {
+        left: 'prev,next',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      editable: false,
+    }
+
+    this.events = [
+      {
+        title: "Evento 1",
+        start: new Date(),
+        description: "Evento 1"
+      },
+      {
+        title: "Evento 2",
+        start: new Date(new Date().getTime() + 86400000),
+        description: "Evento 2"
+      },
+      {
+        title: "Evento 3",
+        start: new Date(new Date().getTime() + (86400000 * 2)),
+        end: new Date(new Date().getTime() + (86400000 * 3)),
+        description: "Evento 3"
+      }
+    ];
+  }
 }
