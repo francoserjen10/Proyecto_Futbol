90506108158b2fe4b5f075432d61ea89465e449b
Franco_Serjen <francoserjen100@gmail.com>
2023-10-18 17:28:36 -0300

    Elimino fullcalendar con sus componentes, tambien primeicons y primeng. En el html de calendario de asistencia elimino el calendario. en el ts tambien borro todo lo que se habia agregado de fullcalendar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { FullCalendarModule } from '@fullcalendar/angular'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,8 +26,7 @@ import { NuevoJugadorComponent } from './nuevo-jugador/nuevo-jugador.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    FullCalendarModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
